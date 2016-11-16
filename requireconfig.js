dde9eba3c69f97b40a76caf6c5431ae2986ac2f6
shusc24 <shusc234@hotmail.com>
2016-11-16 22:31:02 +0800

    Weblib

--- requireconfig.js
+++ requireconfig.js
@@ -1,26 +1,25 @@
 requirejs.config({
-	baseUrl:"lib/Weblib/",
 
 	paths:{
 		//核心库
-		jquery:'public/core/libs/jquery/jquery',
-		Class:"public/core/libs/class/requireClass",
-		underscore:"public/core/libs/underscore/underscore",
-		Vue:"public/core/libs/vue/vue.min",
-		Vuex:"public/core/libs/vue/vuex",
-		core:"public/core/core",
+		jquery:'lib/Weblib/public/core/libs/jquery/jquery',
+		Class:"lib/Weblib/public/core/libs/class/requireClass",
+		underscore:"lib/Weblib/public/core/libs/underscore/underscore",
+		Vue:"lib/Weblib/public/core/libs/vue/vue.min",
+		Vuex:"lib/Weblib/public/core/libs/vue/vuex",
+		core:"lib/Weblib/public/core/core",
 
 		//插件库
-		layer:"public/plus/layer/layer",
-		swiper:"public/plus/swiper/js/swiper.min",
-		echarts:"public/plus/echarts/echarts.min",
-		zui:"public/plus/zui/zui",
-		dynamics:"public/plus/dynamics/dynamics",
+		layer:"lib/Weblib/public/plus/layer/layer",
+		swiper:"lib/Weblib/public/plus/swiper/js/swiper.min",
+		echarts:"lib/Weblib/public/plus/echarts/echarts.min",
+		zui:"lib/Weblib/public/plus/zui/zui",
+		dynamics:"lib/Weblib/public/plus/dynamics/dynamics",
 
 
 		//require插件库
-		text:"public/plus/requirejsplus/text",
-		domReady:"public/plus/requirejsplus/domReady",
+		text:"lib/Weblib/public/plus/requirejsplus/text",
+		domReady:"lib/Weblib/public/plus/requirejsplus/domReady",
 
 		//user插件库
 
