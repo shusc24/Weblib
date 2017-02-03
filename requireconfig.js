e9483437ee0f709e3cbee5dc2b192866a70818bf
shusc24 <shusc234@hotmail.com>
2017-02-03 14:09:06 +0800

    Weblib

--- requireconfig.js
+++ requireconfig.js
@@ -8,6 +8,7 @@ requirejs.config({
         Vuex:"https://cdn.staticfile.org/vuex/2.1.1/vuex.min",
         backbone:"https://cdn.staticfile.org/backbone.js/1.3.3/backbone-min",
         lodash:"https://cdn.staticfile.org/lodash.js/4.17.2/lodash.min",
+        cropper:"https://cdn.staticfile.org/cropper/2.3.4/cropper.min",//图片裁剪插件
 
         core:"lib/Weblib/public/core/core",
         h5core:"lib/Weblib/public/core/H5core",
@@ -44,5 +45,6 @@ requirejs.config({
 		},
         sweetalert:['css!https://cdn.staticfile.org/sweetalert/1.1.3/sweetalert.min.css'],
         swiper2:['css!https://cdn.staticfile.org/Swiper/2.7.6/idangerous.swiper.min.css'],
+        cropper:['jquery','css!https://cdn.staticfile.org/cropper/2.3.4/cropper.min.css'],
 	}
 })
